f6fbeb44d6922a56aa38dd3daa88dd66312fca39
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-02 21:25:30 -0600

    db connection established

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+require('dotenv').config();
+const { testConnection } = require('./db/conn');
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
+
+// test DB connection
+testConnection();
 
 app.get('/health', (req, res) => {
     res.send('I am alive')
